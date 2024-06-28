cbab3310c420c058ccd19994a546b532d3b5f125
Shivanjali <119725141+shivanjalipatil@users.noreply.github.com>
2024-06-28 14:50:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+describe('My Test Suite', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000'); // replace with your site's URL
+  });
+
+  it('should have at least one h1 tag', () => {
+    cy.get('h1').should('have.length.greaterThan', 0);
+  });
+
+  // more tests...
+});
